5e9c756470f5a79c7f56310bc033ab1d927d8dca
arnau <arnaucat@gmail.com>
2019-10-02 14:40:05 +0200

    script con 4 dogs y el metodo

--- dog.py
+++ dog.py
@@ -5,12 +5,26 @@ class Dog:
 	    self.name = name
 	    self.age = age
 
+	def get_biggest_number(*age):
+	    a=max(age)
+	    return a
 
 bambi = Dog("Bambi", 5)
-mikey = Dog("Rufus", 6)
+mikey = Dog("Rufus",16)
+piolin = Dog("Piolin", 8)
+pingu = Dog("Pingu", 10)
+doraemon = Dog("Doraemon", 12)
 
-print("{} is {} and {} is {}.".format(bambi.name, bambi.age, mikey.name, mikey.age))
+
+older = Dog.get_biggest_number(bambi.age,mikey.age,piolin.age,pingu.age,doraemon.age)
+print("The oldest animal has: {}.".format(older))
+print("{} is {} and {} is {} and {} is {} and {} is {} and {} is {}.".format(bambi.name, bambi.age, mikey.name, mikey.age, piolin.name, piolin.age, pingu.name, pingu.age, doraemon.name, doraemon.age))
+
+#print(maxim(bambi.age,mikey.age))
+
+#print (mylist[0].name)
 
 
 if bambi.species == "mammcanicheal":
 	print("{0} is a {1}!".format(bambi.name, bambi.species))
+
